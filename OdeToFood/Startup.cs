c8b6f71869fa4f83677ab3864d6b47c73dfeb515
Cristóbal Tello <cristobal.tello@outlook.com>
2019-08-09 13:22:03 +0200

    02 to 06.Adding a Configuration Source

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -5,12 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +40,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = Configuration["Greeting"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
