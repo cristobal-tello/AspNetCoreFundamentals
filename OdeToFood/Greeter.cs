20cf95158284672352b16471dccbc750d817a47d
Cristóbal Tello <cristobal.tello@outlook.com>
2019-08-09 13:29:59 +0200

    07.Creating a Greeting Service, part 1

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+    public class Greeter : IGreeter
+    {
+        public string GetGreetting()
+        {
+            return "Hello from Greeter!!!";
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -28,10 +24,13 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Take a look 'services' param var has already objects
+
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -40,7 +39,7 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                var message = Configuration["Greeting"];
+                var message = greeter.GetGreetting();
                 await context.Response.WriteAsync(message);
             });
         }
